0f79447524404cc20f8951f66c3591be2332ca83
Mikelraz <mikelrazsol@gmail.com>
2025-07-16 12:37:33 -0400

    Update python modules for linter test v4

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,12 +29,12 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost', 
-    'https://mrazquin-8000.theianext-0-labs-prod-misc-tools-us-east-0.'\
+    'localhost',
+    'https://mrazquin-8000.theianext-0-labs-prod-misc-tools-us-east-0.'
     'proxy.cognitiveclass.ai'
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://mrazquin-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.'\
+    'https://mrazquin-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.'
     'proxy.cognitiveclass.ai'
 ]
 
